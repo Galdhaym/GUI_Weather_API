d7454660aefeeadd89bd5e51d4eae1727ef4f4fd
UnnamedH <55529629+UnnamedH@users.noreply.github.com>
2021-11-09 17:35:45 +0200

    Added fxml support and base start interface

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,11 +1,30 @@
 package com.company;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 
-public class Main {
+public class Main extends Application {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/Main.fxml")));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+
         System.out.println("Прогноз погоды(получение из json колекцию):");
         Forecast forecast = Forecast.getWeatherForecastByURL("https://api.weatherapi.com/v1/forecast.json?key=f720c35b125e4c18a38191356201110&q=Одесса&days=3");
         System.out.println(forecast);
@@ -17,6 +36,5 @@ public class Main {
         System.out.println("Прогноз погоды(фильтрация):");
         List<Day> days = forecast.getForecast().filterByAvgTempGreaterThan(14.2);
         System.out.println(days);
-
     }
 }
