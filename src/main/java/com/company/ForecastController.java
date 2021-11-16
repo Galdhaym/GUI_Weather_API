59433f4bc95f94a2a3bf8ebcf4b0c6532a479645
UnnamedH <55529629+UnnamedH@users.noreply.github.com>
2021-11-16 19:46:10 +0200

    Added progressIndicator to getWeatherForecastByURL function

--- src/main/java/com/company/ForecastController.java
+++ src/main/java/com/company/ForecastController.java
@@ -5,15 +5,13 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class ForecastController {
@@ -89,6 +87,14 @@ public class ForecastController {
         signButton.setValue(">");
     }
 
+    private ProgressIndicator loadProgressIndicator() throws IOException {
+        AnchorPane anchorPane = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/ProgressIndicator.fxml")));
+        ProgressIndicator progressIndicator = (ProgressIndicator) anchorPane.lookup("#progressBar");
+
+        Platform.runLater(() -> containerPane.getChildren().add(progressIndicator));
+        return progressIndicator;
+    }
+
     private void validateIntInput(TextField field, Label label){
         try {
             Integer.parseInt(field.getText());
@@ -116,6 +122,7 @@ public class ForecastController {
 
         Thread thread = new Thread(() -> {
             try {
+                ProgressIndicator progressIndicator = loadProgressIndicator();
                 validateIntInput(daysTextField, daysLabel);
                 String cityValue = cityTextField.getText();
                 int daysValue = Integer.parseInt(daysTextField.getText());
@@ -126,10 +133,9 @@ public class ForecastController {
                         .collect(Collectors.toList());
                 items = FXCollections.observableList(days);
                 forecastTable.setItems(items);
+                Platform.runLater(() -> containerPane.getChildren().remove(progressIndicator));
             } catch (IOException e) {
-                Platform.runLater(() -> {
-                    errorLabel.setText(e.getLocalizedMessage());
-                });
+                Platform.runLater(() -> errorLabel.setText(e.getLocalizedMessage()));
             }
         });
         thread.start();
